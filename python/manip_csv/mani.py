cd8586f888984986b1c9784901d5f927c120ed73
Sandrine LIN <slin@louispion.fr>
2023-12-01 15:57:51 +0100

    batch

--- python/manip_csv/mani.py
+++ python/manip_csv/mani.py
@@ -4,16 +4,16 @@
 import pandas as pd
 
 # ------- GARDER QUE ... QUAND EGALE A ...
-#df = pd.read_csv('C:/Users/slin/Desktop/Script/python/manip_csv/users.csv')
+df = pd.read_csv('C:/Users/slin/Desktop/Script/python/manip_csv/no_auth.csv')
   
 #garder les données égale à "FALSE" dans la colonne nommé "Double_auth"
-#df_new = df[df['Double_auth'] == False]
+df_new = df[df['Double_auth'] == False]
 
 #crée un nouveau fichier 
-#df_new.to_csv('example_3.csv', index=False)
+df_new.to_csv('example_3.csv', index=False)
 
 
 # ------- GARDER QUE SICONTIENT ... 
-df = pd.read_csv('C:/Users/slin/Desktop/Script/python/manip_csv/devices.csv')
-df_new = df[df['Operating system'].astype(str).str.contains("Windows 7")]
-df_new.to_csv('example.csv', index=False)
+# df = pd.read_csv('C:/Users/slin/Desktop/Script/python/manip_csv/devices.csv')
+# df_new = df[df['Operating system'].astype(str).str.contains("Windows 7")]
+# df_new.to_csv('example.csv', index=False)
