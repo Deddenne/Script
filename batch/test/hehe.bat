7e5c0a7df595d5038fb655387ffa0ec717488035
Pinki <slin2@myges.fr>
2023-12-17 23:34:02 +0100

    batch

--- batch/test/hehe.bat
+++ batch/test/hehe.bat
@@ -2,14 +2,16 @@
 
 set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
 
->output-%mydate%.txt (
+(
     for /f "usebackq tokens=1-4 delims=," %%a in ("ouiiiiiii.csv") do (
         echo Poste %%a : %%b
-        IF EXIST not "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting" md "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting"
+        IF NOT EXIST "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting" (
+            md "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting"
+        )
         echo.
         echo --------- copy DLL
-        xcopy /y "\\srv-data\Louis Pion\Informatique\13-Distrib\Nepting" "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting"   
-        timeout 5 > NUL
+        xcopy /y "\\srv-data\Louis Pion\Informatique\13-Distrib\Nepting\*" "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting"   
+        timeout /t 5 > NUL
         echo.
         echo --------- Cegid.CPOS.Nepting.dll
         "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\Cegid Retail Y2 23.0\Front Office\CPOS_DLLManager.exe" -reg "\\%%b\c$\Program Files (x86)\Cegid\Cegid Retail\CPOS Drivers\Nepting\Cegid.CPOS.Nepting.dll"
@@ -19,5 +21,5 @@ set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
         echo.
         echo.
         echo ---------------------------------------
-        )
-)
+    )
+) >output-%mydate%.txt
